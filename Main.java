e51405cf8f4261bbc0cc4af778208d43d220f993
Hubert Jatkowski <hubj1@o2.pl>
2016-04-24 22:01:04 +0200

    Started developing. Simple java window added. TODO: Everything

---
+++ Main.java
@@ -0,0 +1,12 @@
+package JP2PChat;
+import javax.swing.*;
+
+public class Main {
+	public static void main (String args[]) {
+		JFrame mainwindow = new JFrame();
+		JPanel mPanel = new JPanel();
+		mainwindow.add(mPanel);
+		mainwindow.setVisible(true);
+		
+	}
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# -PROZ-JP2PChat
