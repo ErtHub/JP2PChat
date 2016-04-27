e334a59f3433fe193889ba922d165a5228af6068
formateu <formateu@gmail.com>
2016-04-27 21:44:28 +0200

    First fully working version, solved problem of unuseful ports after reconnection. Added proper app close mechanism. TODO: full frontend job - scroll, send with enter, add some format to messages, use swing or something newer than classical awt, because this is ugly, little bit to backend: some optimization

--- Main.java
+++ Main.java
@@ -1,8 +1,11 @@
+import javax.swing.JFrame;
+
 import JP2PChat.MainWindow;
 
 public class Main {
 	public static void main (String args[]) {
 		MainWindow window = new MainWindow();
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setVisible(true);
 		
 	}
--- networking/MessageListener.java
+++ networking/MessageListener.java
@@ -55,6 +55,7 @@ public class MessageListener extends Thread{
 				}
 				
 			}
+			clientSocket.close();
 		}
 		
 		catch (IOException e) {
