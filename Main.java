89e8c23a06f488b1578de0228fb98979a0c8b1eb
Hubert Jatkowski <hubj1@o2.pl>
2016-04-27 18:11:32 +0200

    proper commit of GUI alpha version

--- Main.java
+++ Main.java
@@ -1,12 +1,8 @@
-package JP2PChat;
-import javax.swing.*;
+import JP2PChat.MainWindow;
 
 public class Main {
 	public static void main (String args[]) {
-		JFrame mainwindow = new JFrame();
-		JPanel mPanel = new JPanel();
-		mainwindow.add(mPanel);
-		mainwindow.setVisible(true);
+		MainWindow window = new MainWindow();
 		
 	}
 }
